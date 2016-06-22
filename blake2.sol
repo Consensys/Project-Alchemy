72f238100dca23edf301aaae23be8b4444971d11
Tjaden Hess <tjade273@gmail.com>
2016-06-22 16:22:25 -0400

    Refactor constants to seperate contract

--- blake2.sol
+++ blake2.sol
@@ -1,27 +1,9 @@
 //Line-for-line naieve implementation of IETF RFC 7693
+import "./constants.sol"
+
 contract BLAKE2 {
 
-  uint[16][12] SIGMA = [
-        [  0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15 ],
-        [ 14,10, 4, 8, 9,15,13, 6, 1,12, 0, 2,11, 7, 5, 3 ],
-        [ 11, 8,12, 0, 5, 2,15,13,10,14, 3, 6, 7, 1, 9, 4 ],
-        [  7, 9, 3, 1,13,12,11,14, 2, 6, 5,10, 4, 0,15, 8 ],
-        [  9, 0, 5, 7, 2, 4,10,15,14, 1,11,12, 6, 8, 3,13 ],
-        [  2,12, 6,10, 0,11, 8, 3, 4,13, 7, 5,15,14, 1, 9 ],
-        [ 12, 5, 1,15,14,13, 4,10, 0, 7, 6, 3, 9, 2, 8,11 ],
-        [ 13,11, 7,14,12, 1, 3, 9, 5, 0,15, 4, 8, 6, 2,10 ],
-        [  6,15,14, 9,11, 3, 0, 8,12, 2,13, 7, 1, 4,10, 5 ],
-        [ 10, 2, 8, 4, 7, 6, 1, 5,15,11, 9,14, 3,12,13 ,0 ],
-        [  0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15 ],
-        [ 14,10, 4, 8, 9,15,13, 6, 1,12, 0, 2,11, 7, 5, 3 ]
-    ];
-
-    bytes8[8] IV = [
-        bytes8(0x6a09e667f3bcc908), bytes8(0xbb67ae8584caa73b),
-        bytes8(0x3c6ef372fe94f82b), bytes8(0xa54ff53a5f1d36f1),
-        bytes8(0x510e527fade682d1), bytes8(0x9b05688c2b3e6c1f),
-        bytes8(0x1f83d9abfb41bd6b), bytes8(0x5be0cd19137e2179)
-    ];
+    BLAKE2b_CONST CONST;
 
     function G (bytes8[16] v, uint a, uint b, uint c, uint d, bytes8 x, bytes8 y) returns (bytes8[16]){
       v[a] =bytes8((uint(v[a]) + uint(v[b]) + uint(x)) % 2**64);
@@ -53,7 +35,7 @@ contract BLAKE2 {
 
       for(uint i; i< 8; i++){
         v[i] = h[i];
-        v[8+i] = IV[i];
+        v[8+i] = CONST.IV[i];
       }
 
       v[12] = v[12] ^ bytes8((uint(t) % 2**64));
@@ -62,7 +44,7 @@ contract BLAKE2 {
       if(f) v[14] = v[14] ^ 0xFFFFFFFFFFFFFFFF;
 
       for(i = 0; i < 12; i++){
-        uint[16] s = SIGMA[i%10];
+        uint8[16] s = CONST.SIGMA[i%10];
 
         v = G( v, 0, 4, 8, 12, m[s[0]], m[s[1]]);
         v = G( v, 1, 5, 9, 13, m[s[2]], m[s[3]]);
@@ -85,7 +67,7 @@ contract BLAKE2 {
    // function pad();
 
     function digest(bytes8[8] p, bytes8[16][] d, bytes8[16] key, uint ll) returns(bytes8[8]){
-      bytes8[8] memory h = IV;
+      bytes8[8] memory h = CONST.IV;
       bytes8 nn = p[0];
       bytes8 kk = p[1];
       h[0] = h[0] ^ 0x01010000 ^ shift_left(kk,8) ^ nn;
