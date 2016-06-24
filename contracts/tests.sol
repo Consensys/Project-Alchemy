17edcfe92bc807ceb0220f8c04294b8fbc3f90dc
Tjaden Hess <tjade273@gmail.com>
2016-06-24 18:20:39 -0400

    Fix the test, it passes now!

--- contracts/tests.sol
+++ contracts/tests.sol
@@ -12,7 +12,19 @@ contract BlakeTest is Test {
   }
 
   function testFinalHash (){ //If this works, we're good.....
-    assertTrue(blake.blake2b("", "abc", 64) == 0xBA80A53F981C4D0D6A2797B69F12F6E94C212F14685AC4B74B12BB6FDBFFA2D17D87C5392AAB792DC252D5DE4533CC9518D38AA8DBF1925AB92386EDD4009923);
+    bool correct = true;
+    uint64[8] memory result = blake.blake2b("", "abc", 64);
+    uint64[8] memory trueHash = [0xba80a53f981c4d0d,0x6a2797b69f12f6e9,
+                        0x4c212f14685ac4b7,0x4b12bb6fdbffa2d1,
+                        0x7d87c5392aab792d,0xc252d5de4533cc95,
+                        0x18d38aa8dbf1925a,0xb92386edd4009923];
+    for(uint i; i<8; i++){
+      if(result[i] != trueHash[i]){
+        correct = false;
+      }
+
+      assertTrue(correct);
+    }
   }
 
 }
