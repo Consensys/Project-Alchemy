00082e9947ab65fe2ab032467bb6e199b658a494
Tjaden Hess <tjade273@gmail.com>
2016-06-25 20:00:39 -0400

    Remove debugging events

--- contracts/blake2.sol
+++ contracts/blake2.sol
@@ -32,15 +32,6 @@ contract BLAKE2b {
     uint outlen; //diigest output size
   }
 
-
-  //EVENTS FOR DEBUGGING ONLY
-
-  event Init(uint64[8] h, uint64 c);
-  event PreCompress(uint64[16] v, uint64[16] m);
-  event PostCompress(uint64[16] v);
-  event Update(uint8[128] b, uint64[8] h, uint64[2] t);
-  event H(uint64[8] h);
-
   function G(uint64[16] v, uint a, uint b, uint c, uint d, uint64 x, uint64 y) constant { //OPTIMIZE HERE
        v[a] = v[a] + v[b] + x;
        v[d] = rotate(v[d] ^ v[a], 32);
@@ -77,7 +68,6 @@ contract BLAKE2b {
       m[i] = mi;
     }
 
-    PreCompress(v,m);
 
     for(i=0; i<12; i++){
       G( v, 0, 4, 8, 12, m[SIGMA[i][0]], m[SIGMA[i][1]]);
@@ -88,14 +78,12 @@ contract BLAKE2b {
       G( v, 1, 6, 11, 12, m[SIGMA[i][10]], m[SIGMA[i][11]]);
       G( v, 2, 7, 8, 13, m[SIGMA[i][12]], m[SIGMA[i][13]]);
       G( v, 3, 4, 9, 14, m[SIGMA[i][14]], m[SIGMA[i][15]]);
-      PostCompress(v);
     }
 
 
     for(i=0; i<8; ++i){
       ctx.h[i] = ctx.h[i] ^ v[i] ^ v[i+8];
     }
-    H(ctx.h);
   }
 
   function init(BLAKE2b_ctx ctx, uint64 outlen, bytes key) private{
@@ -125,7 +113,6 @@ contract BLAKE2b {
         update(ctx, key);
         ctx.c = 128;
       }
-      Init(ctx.h, ctx.c);
 
   }
 
@@ -144,7 +131,6 @@ contract BLAKE2b {
 
       ctx.b[ctx.c++] = uint8(input[i]);
 
-      Update(ctx.b, ctx.h, ctx.t);
     }
   }
 
