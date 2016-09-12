9e8fafecb40b4fcf4d19b5ca8a860dc9aa947c1a
Tjaden Hess <tjade273@gmail.com>
2016-09-12 16:26:15 -0400

    Fix testing logic

--- contracts/EquihashTests.sol
+++ contracts/EquihashTests.sol
@@ -21,7 +21,7 @@ contract EquihashTest is Test {
     bytes memory seed = "Equihash is an asymmetric PoW based on the Generalised Birthday problem.";
     uint nonce = 1;
     uint32[32] memory soln = [uint32(2154), uint32(87055), uint32(7922), uint32(12920), uint32(45189), uint32(49783), uint32(122795), uint32(124296), uint32(2432), uint32(48178), uint32(48280), uint32(67880), uint32(3912), uint32(62307), uint32(10987), uint32(93891), uint32(19673), uint32(24483), uint32(33984), uint32(91500), uint32(38171), uint32(85505), uint32(94625), uint32(106140), uint32(31530), uint32(60861), uint32(59391), uint32(117337), uint32(68078), uint32(129665), uint32(126764), uint32(128278)];
-    uint32[] memory s;
+    uint32[] memory s = new uint32[](32);
     for(uint i=0; i<32; i++){
       s[i] = soln[i];
     }
