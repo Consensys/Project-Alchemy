25edabee3a4d9f7017725c7df34f0e9e92881846
Tjaden Hess <tjade273@gmail.com>
2016-09-23 12:53:58 -0400

    Factor constants out

--- contracts/blake2.sol
+++ contracts/blake2.sol
@@ -1,32 +1,7 @@
 import "./GasTest.sol";
+import "./constants.sol";
 
-contract BLAKE2b is GasTest{
-
-  /*
-  Constants, as defined in RFC 7693
-  */
-  uint8[16][12] public SIGMA = [
-        [  0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15 ],
-        [ 14,10, 4, 8, 9,15,13, 6, 1,12, 0, 2,11, 7, 5, 3 ],
-        [ 11, 8,12, 0, 5, 2,15,13,10,14, 3, 6, 7, 1, 9, 4 ],
-        [  7, 9, 3, 1,13,12,11,14, 2, 6, 5,10, 4, 0,15, 8 ],
-        [  9, 0, 5, 7, 2, 4,10,15,14, 1,11,12, 6, 8, 3,13 ],
-        [  2,12, 6,10, 0,11, 8, 3, 4,13, 7, 5,15,14, 1, 9 ],
-        [ 12, 5, 1,15,14,13, 4,10, 0, 7, 6, 3, 9, 2, 8,11 ],
-        [ 13,11, 7,14,12, 1, 3, 9, 5, 0,15, 4, 8, 6, 2,10 ],
-        [  6,15,14, 9,11, 3, 0, 8,12, 2,13, 7, 1, 4,10, 5 ],
-        [ 10, 2, 8, 4, 7, 6, 1, 5,15,11, 9,14, 3,12,13 ,0 ],
-        [  0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15 ],
-        [ 14,10, 4, 8, 9,15,13, 6, 1,12, 0, 2,11, 7, 5, 3 ]
-    ];
-
-    uint64[8] public IV = [
-        0x6a09e667f3bcc908, 0xbb67ae8584caa73b,
-        0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1,
-        0x510e527fade682d1, 0x9b05688c2b3e6c1f,
-        0x1f83d9abfb41bd6b, 0x5be0cd19137e2179
-    ];
-
+contract BLAKE2b is GasTest, BLAKE2_Constants{
 
   struct BLAKE2b_ctx {
     uint256[4] b; //input buffer
@@ -215,16 +190,15 @@ contract BLAKE2b is GasTest{
 // Utility functions
 
   //Flips endianness of words
-  //TODO: Use constant variables for readablitity
   function getWords(uint64 a) constant returns (uint64 b) {
-    return  (a & 0xFF00000000000000) /0x0100000000000000^
-            (a & 0x00FF000000000000) /0x0000010000000000^
-            (a & 0x0000FF0000000000) /0x0000000001000000^
-            (a & 0x000000FF00000000) /0x0000000000000100^
-            (a & 0x00000000FF000000) *0x0000000000000100^
-            (a & 0x0000000000FF0000) *0x0000000001000000^
-            (a & 0x000000000000FF00) *0x0000010000000000^
-            (a & 0x00000000000000FF) *0x0100000000000000;
+    return  (a & MASK_0) / SHIFT_0 ^
+            (a & MASK_1) / SHIFT_1 ^
+            (a & MASK_2) / SHIFT_2 ^
+            (a & MASK_3) / SHIFT_3 ^
+            (a & MASK_4) * SHIFT_3 ^
+            (a & MASK_5) * SHIFT_2 ^
+            (a & MASK_6) * SHIFT_1 ^
+            (a & MASK_7) * SHIFT_0;
   }
 
   function shift_right(uint64 a, uint shift) constant returns(uint64 b){
